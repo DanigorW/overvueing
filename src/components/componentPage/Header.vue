8c99846b240b223d3376d281063540a52b8c3bb9
Maryanna <maryannab@affilomania.com>
2022-01-11 13:36:27 +0200

    test

--- src/components/componentPage/Header.vue
+++ src/components/componentPage/Header.vue
@@ -9,6 +9,7 @@
         The building blocks of any framework, in vue you can create reusable components and use them where ever you like, you can create more complex components that can be controlled via props and change the behavior and run different logic if needed - think about normal webpage/crm/cms - you can have multiple buttons that do different actions so in that case the better approach is to create button component that you can control the behavior and logic by adding props.
         "
       />
+      <div>MEOW</div>
     </div>
   </div>
 </template>
