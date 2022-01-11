e48e7d38bb4f7a2a7a3f6ceb73c545205e226a2d
igor lev <{igorleumi@gmail.com}>
2022-01-11 08:29:41 +0200

    added titles

--- src/router/index.js
+++ src/router/index.js
@@ -8,91 +8,91 @@ const routes = [{
         path: '/',
         name: 'Home',
         component: () =>
-            import( /* webpackChunkName: "Home" */ '../views/Home.vue')
+            import ( /* webpackChunkName: "Home" */ '../views/Home.vue')
     },
     {
         path: '/installation',
         name: 'Installation',
         component: () =>
-            import( /* webpackChunkName: "installation" */ '../views/Installation.vue')
+            import ( /* webpackChunkName: "installation" */ '../views/Installation.vue')
     },
     {
         path: '/plugins',
         name: 'Plugins',
         component: () =>
-            import( /* webpackChunkName: "Plugins" */ '../views/Plugins.vue')
+            import ( /* webpackChunkName: "Plugins" */ '../views/Plugins.vue')
     },
     {
         path: '/state',
         name: 'State',
         component: () =>
-            import( /* webpackChunkName: "State" */ '../views/State.vue')
+            import ( /* webpackChunkName: "State" */ '../views/State.vue')
     },
     {
         path: '/routes',
         name: 'Routes',
         component: () =>
-            import( /* webpackChunkName: "Routes" */ '../views/Routes.vue')
+            import ( /* webpackChunkName: "Routes" */ '../views/Routes.vue')
     },
     {
         path: '/components',
         name: 'Components',
         component: () =>
-            import( /* webpackChunkName: "Routes" */ '../views/Components.vue')
+            import ( /* webpackChunkName: "Routes" */ '../views/Components.vue')
     },
     {
         path: '/props',
         name: 'Props',
         component: () =>
-            import( /* webpackChunkName: "Props" */ '../views/Props.vue')
+            import ( /* webpackChunkName: "Props" */ '../views/Props.vue')
     },
     {
         path: '/lifecycle',
         name: 'Lifecycle',
         component: () =>
-            import( /* webpackChunkName: "Lifecycle" */ '../views/Lifecycle.vue')
+            import ( /* webpackChunkName: "Lifecycle" */ '../views/Lifecycle.vue')
     },
     {
         path: '/watchers',
         name: 'Watchers',
         component: () =>
-            import( /* webpackChunkName: "Watchers" */ '../views/Watchers.vue')
+            import ( /* webpackChunkName: "Watchers" */ '../views/Watchers.vue')
     },
     {
         path: '/mixins',
         name: 'Mixins',
         component: () =>
-            import( /* webpackChunkName: "Mixins" */ '../views/Mixins.vue')
+            import ( /* webpackChunkName: "Mixins" */ '../views/Mixins.vue')
     },
     {
         path: '/filters',
         name: 'Filters',
         component: () =>
-            import( /* webpackChunkName: "Filters" */ '../views/Filters.vue')
+            import ( /* webpackChunkName: "Filters" */ '../views/Filters.vue')
     },
     {
         path: '/directives',
         name: 'Directives',
         component: () =>
-            import( /* webpackChunkName: "Directives" */ '../views/Directives.vue')
+            import ( /* webpackChunkName: "Directives" */ '../views/Directives.vue')
     },
     {
         path: '/fetch',
         name: 'Fetch',
         component: () =>
-            import( /* webpackChunkName: "Fetch" */ '../views/Fetch.vue')
+            import ( /* webpackChunkName: "Fetch" */ '../views/Fetch.vue')
     },
     {
         path: '/forms',
         name: 'Forms',
         component: () =>
-            import( /* webpackChunkName: "Forms" */ '../views/Forms.vue')
+            import ( /* webpackChunkName: "Forms" */ '../views/Forms.vue')
     },
     {
         path: '/computed',
         name: 'Computed',
         component: () =>
-            import( /* webpackChunkName: "Computed" */ '../views/Computed.vue')
+            import ( /* webpackChunkName: "Computed" */ '../views/Computed.vue')
     },
 
     {
@@ -126,6 +126,9 @@ const router = new VueRouter({
 })
 
 
-
+router.beforeEach((to, from, next) => {
+    document.title = 'overvueing - ' + to.name;
+    next();
+});
 
 export default router
